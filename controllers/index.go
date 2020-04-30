74ea6fc285c026d08f145540d34b524206b899ab
Jean-Philippe <jean.philippe.mench@me.com>
2020-04-30 16:01:17 +0200

    Snappass 🔐 : Update Echo.

--- controllers/index.go
+++ controllers/index.go
@@ -3,7 +3,7 @@ package controllers
 import (
 	. "github.com/jeanphilippe-mh/Okuru/models"
 	. "github.com/jeanphilippe-mh/Okuru/utils"
-	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo"
 	log "github.com/sirupsen/logrus"
 	"net/http"
 	"strconv"
