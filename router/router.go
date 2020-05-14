ea417c4e1808d4074528309eb7d0a72f69c6bfda
Jean-Philippe <jean.philippe.mench@me.com>
2020-05-14 17:52:43 +0200

    Snappass 🔐 : Update Echo to the latest version (4.1.16).

--- router/router.go
+++ router/router.go
@@ -9,7 +9,7 @@ import (
 
 	"github.com/flosch/pongo2"
 	"github.com/labstack/echo"
-	"github.com/labstack/echo/middleware"
+	"github.com/labstack/echo/v4/middleware"
 	"gopkg.in/go-playground/validator.v9"
 	"io"
 )
