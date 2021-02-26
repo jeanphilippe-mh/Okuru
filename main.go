a954989747d6b2a5b7d0da66b33f7eab78e0baf4
Jean-Philippe <jean.philippe.mench@me.com>
2021-02-26 23:50:33 +0100

    Snappass 🔐 : Add goroutine CleanFileWatch in init.

--- main.go
+++ main.go
@@ -44,7 +44,7 @@ func init() {
 		log.SetLevel(log.WarnLevel)
 	}
 	
-	go CleanFileWatch(c)
+	go CleanFileWatch(context.Context, string, func() error, func(string, []byte) error, ...string)
 }
 
 func main() {
