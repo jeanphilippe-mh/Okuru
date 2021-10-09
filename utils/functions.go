2911fb47d31af4720cdeaec32c1ddef6e2c97362
Jean-Philippe <jean.philippe.mench@me.com>
2021-10-09 19:07:38 +0200

    nappass 🔐 : Generate secure random strings in golang with crypto/rand.

--- utils/functions.go
+++ utils/functions.go
@@ -425,7 +425,7 @@ func CleanFile(fileName string) {
 // Source: https://gist.github.com/dopey/c69559607800d2f2f90b1b1ed4e550fb
 func GenerateRandomString(n int) (string, error) {
 	const letters = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz-"
-	ret := make([]byte, n)
+	b := make([]byte, n)
 	for i := 0; i < n; i++ {
 		num, err := rand.Int(rand.Reader, big.NewInt(int64(len(letters))))
 		if err != nil {
