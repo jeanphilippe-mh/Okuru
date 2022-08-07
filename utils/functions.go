54b5c94db555f4b06604af9f031477711ab1ea98
Jean-Philippe <jean.philippe.mench@me.com>
2022-08-07 12:48:51 +0200

    Snappass 🔐 : Fix Uncontrolled data used in path expression (Code scanning alert).

--- utils/functions.go
+++ utils/functions.go
@@ -48,7 +48,7 @@ func inTrustedRoot(path string, trustedRoot string) error {
 	return errors.New("path is outside of trusted root")
 }
 
-func verifyPath(path string) (string) {
+func verifyPath(path string) string {
 
 	// Read from FILEFOLDER .env configuration
 	trustedRoot := "FILEFOLDER"
@@ -65,10 +65,8 @@ func verifyPath(path string) (string) {
 	if err != nil {
 		fmt.Println("Error " + err.Error())
 		log.Error("unsafe or invalid path specified", err)
-	} else {
-		return r
 	}
-	return errors.New("unsafe or invalid path specified")
+	return path
 }
 
 /**
