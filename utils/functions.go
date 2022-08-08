e56af685e75aedd2e0d605372ec2625b20524d04
Jean-Philippe <jean.philippe.mench@me.com>
2022-08-08 23:24:04 +0200

    Snappass 🔐 : Revert Fix Uncontrolled data used in path expression (Code scanning alert).

--- utils/functions.go
+++ utils/functions.go
@@ -3,11 +3,9 @@ package utils
 import (
 	"crypto/rand"
 	"errors"
-	"fmt"
 	"math/big"
 	"net/http"
 	"os"
-	"path/filepath"
 	"strconv"
 	"strings"
 	"time"
@@ -33,40 +31,6 @@ func GetBaseUrl(context echo.Context) string {
 		url = currentURL
 	}
 	return url
-}
-
-/**
- * Establish a Trusted Root for /File.
- */
-func inTrustedRoot(path string, trustedRoot string) error {
-	for path != "/" {
-		path = filepath.Dir(path)
-		if path == trustedRoot {
-			return nil
-		}
-	}
-	return errors.New("path is outside of trusted root")
-}
-
-func verifyPath(path string) string {
-
-	// Read from FILEFOLDER .env configuration
-	trustedRoot := FILEFOLDER
-	
-	r, err := filepath.EvalSymlinks(path)
-	if err != nil {
-		fmt.Println("Error " + err.Error())
-		log.Error("unsafe or invalid path specified", err)
-	}
-
-	err = inTrustedRoot(r, trustedRoot)
-	if err != nil {
-		fmt.Println("Error " + err.Error())
-		log.Error("unsafe or invalid path specified", err)
-	} else {
-		return r
-	}
-	return r
 }
 
 /**
