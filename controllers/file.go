623e2aa4d38355f1ed9fbfa59422560890df15be
Jean-Philippe <jean.philippe.mench@me.com>
2023-02-11 22:46:53 +0100

    Snappass 🔐 : Secure Uncontrolled data used in path expression (Code scanning Alert).

--- controllers/file.go
+++ controllers/file.go
@@ -238,10 +238,15 @@ func AddFile(context echo.Context) error {
 		}
 		totalUploadedFileSize += file.Size
 
+		// Replace newline characters to prevent path traversal attacks
+		escapedfileName := strings.ReplaceAll(fileName, "\n", "")
+		escapedfileName = strings.ReplaceAll(escapedfileName, "\r", "")
+		log.Debug("CleanFile fileName : %s\n", escapedfileName)
+		
 		// Validate the file name
 		fileNamePattern := `^[a-zA-Z0-9_-]+\.[a-zA-Z0-9]+$`
 		re := regexp.MustCompile(fileNamePattern)
-		cleanFileName := filepath.Base(file.Filename)
+		cleanFileName := filepath.Base(escapedfileName)
 		
 		if !re.MatchString(cleanFileName) {
 		errorMessage := "File name contains prohibited characters"
@@ -289,7 +294,7 @@ func AddFile(context echo.Context) error {
 			return context.Render(http.StatusOK, "index_file.html", DataContext)
 		}
 
-		fileList = append(fileList, folderPathName+file.Filename)
+		fileList = append(fileList, dstPath)
 	}
 
 	if totalUploadedFileSize > MaxFileSize {
