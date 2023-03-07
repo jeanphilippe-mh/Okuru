a5ae0275a53fa183f987683dd704a8f18b8f6c71
Jean-Philippe <jean.philippe.mench@me.com>
2023-03-07 23:58:17 +0100

    Snappass 🔐 : Add Middleware CSRF support in router.go (OWASP ZAP).

--- router/router.go
+++ router/router.go
@@ -101,6 +101,7 @@ func New() *echo.Echo {
 	
 	// Middleware CSRF
 	e.Use(middleware.CSRFWithConfig(middleware.CSRFConfig{
+		TokenLenght:	32,
 		TokenLookup:    "cookie:_csrf",
 		CookieSecure:	true,
 		CookieHTTPOnly:	true,
