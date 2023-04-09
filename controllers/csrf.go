e75fdbcac9c0812f72438cb04e12be699ce66592
Jean-Philippe <jean.philippe.mench@me.com>
2023-04-09 18:39:07 +0200

    Snappass 🔐 : Add CSRF controller to verify HTML submission form (OWASP ZAP).

--- controllers/csrf.go
+++ controllers/csrf.go
@@ -1,12 +1,19 @@
 package controllers
 
 import (
+	"net/http"
+	"log"
 	"github.com/labstack/echo/v4"
 )
 
-func GetCSRFToken(c echo.Context) error {
-	token := c.Get("csrf").(string)
-	return c.JSON(200, map[string]string{
-		"csrfToken": token,
-	})
+func getCSRFToken(c echo.Context) error {
+    delete(DataContext, "errors")
+    csrfToken := c.Get("csrf_token")
+
+    if csrfToken == nil {
+        err := errors.New("Failed to retrieve CSRF token")
+        log.Error("%+v\n", err)
+        return c.Render(http.StatusBadRequest, "403.html", DataContext)
+    }
+    return c.JSON(http.StatusOK, map[string]string{"csrfToken": csrfToken.(string)})
 }
