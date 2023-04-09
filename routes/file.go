21c252b322c81b7efa480cde7b1e6f55b02e82e5
Jean-Philippe <jean.philippe.mench@me.com>
2023-04-09 22:05:55 +0200

    Snappass 🔐 : Update file.go and remove /csrf-token route to handle CSRF implementation.

--- routes/file.go
+++ routes/file.go
@@ -9,7 +9,6 @@ func File(g *echo.Group) {
 	g.GET("", controllers.IndexFile)
 	g.GET("/remove/:file_key", controllers.DeleteFile)
 	g.GET("/:file_key", controllers.ReadFile)
-	g.GET("/csrf-token", controllers.GetCSRFToken)
 	g.POST("/:file_key", controllers.DownloadFile)
 	g.POST("", controllers.AddFile)
 	g.DELETE("/:file_key", controllers.DeleteFile)
