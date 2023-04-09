59e7d121ff2808be37387b58ecd6d94662aa1208
Jean-Philippe <jean.philippe.mench@me.com>
2023-04-09 22:05:12 +0200

    Snappass 🔐 : Update index.go and remove /csrf-token route to handle CSRF implementation.

--- routes/index.go
+++ routes/index.go
@@ -10,7 +10,6 @@ func Index(e *echo.Echo) {
 	e.POST("/", controllers.AddIndex)
 	e.GET("/privacy-policy", controllers.PrivacyIndex)
 	e.GET("/:password_key", controllers.ReadIndex)
-	e.GET("/csrf-token", controllers.GetCSRFToken)
 	e.POST("/:password_key", controllers.RevealPassword)
 	e.GET("/remove/:password_key", controllers.DeleteIndex)
 }
