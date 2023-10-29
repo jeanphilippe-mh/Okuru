0f4b7a940b28235da44cb0d05208dd421eca1ba0
Jean-Philippe <jean.philippe.mench@me.com>
2023-10-29 22:29:12 +0100

    Snappass 🔐 : Add security.txt controller redirection to /.well-known/security.txt.

--- routes/index.go
+++ routes/index.go
@@ -9,6 +9,7 @@ func Index(e *echo.Echo) {
 	e.GET("/", controllers.Index)
 	e.POST("/", controllers.AddIndex)
 	e.GET("/privacy-policy", controllers.PrivacyIndex)
+	e.GET("/security.txt", controllers.SecurityIndex)
 	e.GET("/:password_key", controllers.ReadIndex)
 	e.POST("/:password_key", controllers.RevealPassword)
 	e.GET("/remove/:password_key", controllers.DeleteIndex)
