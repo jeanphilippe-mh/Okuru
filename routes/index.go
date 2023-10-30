c913951c063e3b4e005f021ae8e7affb142f8d1c
Jean-Philippe <jean.philippe.mench@me.com>
2023-10-30 11:52:30 +0100

    Snappass 🔐 : Add 40x route errors for index.go controller redirection.

--- routes/index.go
+++ routes/index.go
@@ -13,7 +13,7 @@ func Index(e *echo.Echo) {
 	e.GET("/403.html", controllers.Error403Index)
 	e.GET("/404.html", controllers.Error404Index)
 	e.GET("/413.html", controllers.Error413Index)
-	e.GET("/500/html", controllers.Error500Index)
+	e.GET("/500.html", controllers.Error500Index)
 	e.GET("/security.txt", controllers.SecurityIndex)
 	e.GET("/:password_key", controllers.ReadIndex)
 	e.POST("/:password_key", controllers.RevealPassword)
