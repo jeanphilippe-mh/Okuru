7d3a2fe0d0d2e271ff6fa125b29ff5e83fe35403
Jean-Philippe <jean.philippe.mench@me.com>
2023-11-05 22:58:01 +0100

    Snappass 🔐 : Add 40x route errors for file.go controller redirection.

--- routes/file.go
+++ routes/file.go
@@ -7,11 +7,11 @@ import (
 
 func File(g *echo.Group) {
 	g.GET("", controllers.IndexFile)
-	e.GET("/400.html", controllers.Error400File)
-	e.GET("/403.html", controllers.Error403File)
-	e.GET("/404.html", controllers.Error404File)
-	e.GET("/413.html", controllers.Error413File)
-	e.GET("/500.html", controllers.Error500File)
+	g.GET("/400.html", controllers.Error400File)
+	g.GET("/403.html", controllers.Error403File)
+	g.GET("/404.html", controllers.Error404File)
+	g.GET("/413.html", controllers.Error413File)
+	g.GET("/500.html", controllers.Error500File)
 	g.GET("/remove/:file_key", controllers.DeleteFile)
 	g.GET("/:file_key", controllers.ReadFile)
 	g.POST("/:file_key", controllers.DownloadFile)
