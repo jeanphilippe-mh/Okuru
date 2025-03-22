d9e59cf3ea291d6e056e99007f6a1178a35400a4
Jean-Philippe <jean.philippe.mench@me.com>
2025-03-22 12:06:50 +0100

    Snappass 🔐 : Add refinements for File.go data model.

--- models/File.go
+++ models/File.go
@@ -9,7 +9,7 @@ type File struct {
 
 	TTL        int `json:"ttl,omitempty" xml:"ttl,omitempty" form:"ttl,omitempty" query:"ttl,omitempty" redis:"ttl,omitempty"`
 	Views      int `json:"views,omitempty" xml:"views,omitempty" form:"views,omitempty" query:"views,omitempty" redis:"views,omitempty"`
-	ViewsCount int `redis:"views_count,omitempty"`
+	ViewsCount int `json:"views_count,omitempty" redis:"views_count,omitempty"`
 
 	Deletable bool   `json:"deletable,omitempty" xml:"deletable,omitempty" form:"deletable,omitempty" query:"deletable,omitempty" redis:"deletable,omitempty"`
 	FileKey   string `json:"file_key,omitempty" xml:"file_key,omitempty" form:"file_key,omitempty" query:"file_key,omitempty"`
